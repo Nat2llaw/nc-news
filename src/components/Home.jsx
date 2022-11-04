5447216b9b37ebe19983aeefc926f515b7118059
Nathaniel <nat2llaw@gmail.com>
2022-11-04 15:18:51 +0000

    remove home text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,7 @@ import Articles from './article/Articles'
 
 const Home = () => {
   return (
-      <div>
-      <h1>This is the best website for your daily news</h1>
       <Articles />
-    </div>
   )
 }
 
