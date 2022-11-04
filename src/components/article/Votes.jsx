53cb013dd563a1168af97293c3af80674c55df83
Nathaniel <nat2llaw@gmail.com>
2022-11-04 14:26:02 +0000

    change votes

--- src/components/article/Votes.jsx
+++ src/components/article/Votes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./votes.css"
+import "./votes.css";
 import { useState, useEffect } from "react";
 import { changeApiArticleVotes } from "../../utils/Api";
 import { useParams } from "react-router-dom";
@@ -7,38 +7,49 @@ import { useParams } from "react-router-dom";
 const Votes = ({ votes }) => {
   const [vote, setVote] = useState(0);
   const [totalVotes, setTotalVotes] = useState(votes);
+  const [disable, setDisable] = useState(false);
   const { id } = useParams();
 
-  const changeVote = (event) => {
+  const incVote = (event) => {
     event.preventDefault();
-    const votechange = event.target.input.value;
-    setVote(votechange);
+    setDisable(true);
+    setVote((currVote) => {
+      return currVote + 1;
+    });
   };
-    useEffect(() => {
-      changeApiArticleVotes(id, vote).then((res) => {
-        setTotalVotes(res.article.votes);
-      });  
-    }, [vote, id])
-  
+  const decVote = (event) => {
+    event.preventDefault();
+    setDisable(true);
+    setVote((currVote) => {
+      return currVote - 1;
+    });
+  };
+  useEffect(() => {
+    changeApiArticleVotes(id, vote).then((res) => {
+      setTotalVotes(res.article.votes);
+    });
+  }, [vote, id]);
+
   return (
     <>
       <div className="voteContainer">
-        <form onSubmit={changeVote}>
-          <fieldset className="voteBox">
-            <legend className="voteTitle">Change Vote</legend>
-            <div>
-              <label htmlFor="input" className="field">
-                input a number:
-              </label>
-              <input name="input" type="number" />
-            <button className="submit" type="submit">
-              vote
-              </button>
-            </div>
-            <h2>Votes: {totalVotes} </h2>
-            <h2>Changed by: {vote}</h2>
-          </fieldset>
-        </form>
+        <button
+          className="submitVote"
+          type="submit"
+          disabled={disable}
+          onClick={incVote}
+        >
+          👍
+        </button>
+        <h2>Votes: {totalVotes} </h2>
+        <button
+          className="submitVote"
+          type="submit"
+          disabled={disable}
+          onClick={decVote}
+        >
+          👎
+        </button>
       </div>
     </>
   );
